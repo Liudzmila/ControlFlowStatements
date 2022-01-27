60946cf0ba5cb814cf748efa70b81adb140a3bda
Liudzmila Sobaleva <lusya88@gmail.com>
2022-01-27 11:09:16 +0300

    Added Main

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //выводим числа от 0 до 100, кратные 4
+
+       //the looping statement for
+        for (int a = 0; a <= 100; a++) {
+            if (a % 4 == 0)
+            System.out.println(a);
+        }
+
+        //the looping statement while
+        int b = 0;
+        while (b <= 100) {
+            if (b % 4 == 0)
+            System.out.println(b);
+            b++;
+        }
+
+        //the looping statement do-while
+        int c = 0;
+        do { if (c % 4 == 0)
+            System.out.println(c);
+            c++;
+        } while (c <= 100);
+
+        }
+
+    }
+
